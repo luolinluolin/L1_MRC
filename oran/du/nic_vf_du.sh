2a78676d39159ad542fa5cc1b113673bd059c675
Luo, Lin <lin.luo@intel.com>
2022-02-23 09:31:18 -0800

    debug ru

--- oran/du/nic_vf_du.sh
+++ oran/du/nic_vf_du.sh
@@ -7,13 +7,13 @@ NID=` ethtool -i ${data_nic}|grep bus-info|cut -c16-26|awk -F: '{print $1}' `
 echo "this is NIC NID number ==${NID}"
 echo 6 > /sys/bus/pci/devices/0000\:${NID}\:00.0/sriov_numvfs
 
-ip link set ${data_nic} vf 1 mac 00:11:22:33:00:10 vlan 1
-ip link set ${data_nic} vf 0 mac 00:11:22:33:00:00 vlan 2
+ip link set ${data_nic} vf 0 mac 00:11:22:33:00:00 vlan 1
+ip link set ${data_nic} vf 1 mac 00:11:22:33:00:10 vlan 2
 
-ip link set ${data_nic} vf 3 mac 00:11:22:33:00:30 vlan 1
-ip link set ${data_nic} vf 2 mac 00:11:22:33:00:20 vlan 2
+ip link set ${data_nic} vf 2 mac 00:11:22:33:00:20 vlan 3
+ip link set ${data_nic} vf 3 mac 00:11:22:33:00:30 vlan 4
 
-ip link set ${data_nic} vf 5 mac 00:11:22:33:01:10 vlan 1
-ip link set ${data_nic} vf 4 mac 00:11:22:33:01:00 vlan 2
+ip link set ${data_nic} vf 4 mac 00:11:22:33:01:40 vlan 5
+ip link set ${data_nic} vf 5 mac 00:11:22:33:01:50 vlan 6
 
 ip link  show
