9df9d325c921b444e121530581b12923893637f1
luolinluolin <lin.luo@intel.com>
2022-02-27 09:16:27 -0800

    enable more test case

--- oran/du/sub3_mu0_20mhz_sub6_mu1_100mhz_4x4/run.sh
+++ oran/du/sub3_mu0_20mhz_sub6_mu1_100mhz_4x4/run.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-work_path=$FLEXRAN_L1_SW
-./update_conf.sh
-rm -rf $work_path/*.log
 
-./l1_5.ex $work_path &
+./update_conf.sh
+BASE=$PWD
+l1_sw=$FLEXRAN_L1_SW
+
+rm -rf $BASE/*.log
+
+./l1_5.ex $BASE $l1_sw&
 
 sleep 20
 
-./l2_5.ex $work_path 
+./l2_5.ex $BASE $l1_sw
 
-# ./kill.ex $PWD
+# ./kill.ex $work_path
+
 echo "this DU run script  is done "
-
