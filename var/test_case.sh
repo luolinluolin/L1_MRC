617d233e2a234f7372dc614784459c63ded52202
luolinluolin <lin.luo@intel.com>
2022-02-28 16:03:39 -0800

    fix json issue

--- var/test_case.sh
+++ var/test_case.sh
@@ -8,22 +8,22 @@ html_name_diff=`cat $cfgfile |$JQ '.[0]|.html_name_diff'`
 cfile_name_diff=`cat $cfgfile |$JQ '.[0]|.cfile_name_diff'`
 
 #parse test case
-timer_csl_sp_case=`(cat $cfgfile |$JQ '.[0]|.timer_csl_sp_info|.[]|.case')`
+timer_csl_sp_case=`(cat $cfgfile |$JQ '.[0]|.timer_csl_sp_info|.[]|.case' |sed 's/\"//g')`
 timer_csl_sp_info=`(cat $cfgfile |$JQ '.[0]|.timer_csl_sp_info|.[]|.info')`
 timer_csl_sp_cfile=`(cat $cfgfile |$JQ '.[0]|.timer_csl_sp_info|.[]|.cfile')`
 
-timer_icl_sp_case=`(cat $cfgfile |$JQ '.[0]|.timer_icl_sp_info|.[]|.case')`
+timer_icl_sp_case=`(cat $cfgfile |$JQ '.[0]|.timer_icl_sp_info|.[]|.case' |sed 's/\"//g')`
 timer_icl_sp_info=`(cat $cfgfile |$JQ '.[0]|.timer_icl_sp_info|.[]|.info')`
 timer_icl_sp_cfile=`(cat $cfgfile |$JQ '.[0]|.timer_icl_sp_info|.[]|.cfile')`
 
-timer_icl_d_case=`(cat $cfgfile |$JQ '.[0]|.timer_icl_d_info|.[]|.case')`
+timer_icl_d_case=`(cat $cfgfile |$JQ '.[0]|.timer_icl_d_info|.[]|.case' |sed 's/\"//g')`
 timer_icl_d_info=`(cat $cfgfile |$JQ '.[0]|.timer_icl_d_info|.[]|.info')`
 timer_icl_d_cfile=`(cat $cfgfile |$JQ '.[0]|.timer_icl_d_info|.[]|.cfile')`
 
-oran_csl_sp_case=`(cat $cfgfile |$JQ '.[0]|.oran_csl_sp_info|.[]|.case')`
+oran_csl_sp_case=`(cat $cfgfile |$JQ '.[0]|.oran_csl_sp_info|.[]|.case' |sed 's/\"//g')`
 oran_csl_sp_info=`(cat $cfgfile |$JQ '.[0]|.oran_csl_sp_info|.[]|.info')`
 oran_csl_sp_cfile=`(cat $cfgfile |$JQ '.[0]|.oran_csl_sp_info|.[]|.cfile')`
 
-oran_icl_sp_case=`(cat $cfgfile |$JQ '.[0]|.oran_icl_sp_info|.[]|.case')`
+oran_icl_sp_case=`(cat $cfgfile |$JQ '.[0]|.oran_icl_sp_info|.[]|.case' |sed 's/\"//g')`
 oran_icl_sp_info=`(cat $cfgfile |$JQ '.[0]|.oran_icl_sp_info|.[]|.info')`
 oran_icl_sp_cfile=`(cat $cfgfile |$JQ '.[0]|.oran_icl_sp_info|.[]|.cfile')`
