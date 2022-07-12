2afba3620ec9741ec9d0da9deb9819820799ca28
Luo, Lin <lin.luo@intel.com>
2022-07-12 01:46:11 +0000

    update setup

--- setup/mbc_vc_setup.sh
+++ setup/mbc_vc_setup.sh
@@ -44,22 +44,8 @@ cd ${BASE}/inih/; meson build; cd build; ninja; cp -rf lib* ${bbdev_config}/inih
 cd ${bbdev_config}; make clean;make
 
 #######################################################################################
-cd $BASE
-if [ ! -d dpdk-kmods ];then
-  git clone http://dpdk.org/git/dpdk-kmods
-fi
-
-if [ ! -f $BASE/dpdk-kmods/linux/igb_uio/igb_uio.ko ]
-then
-   echo "........start to build igb ko file ...."
-   cd $BASE/dpdk-kmods/linux/igb_uio/; make clean;make
-fi
-
-if [ `lsmod |awk '{print $1}'|grep igb_uio|wc -l` -eq 0 ]
-then
-    modprobe uio
-    insmod  $BASE/dpdk-kmods/linux/igb_uio/igb_uio.ko
-fi
+echo "-------------install igb uio---------------"
+sh ${CURRENT_DIR}/igb_uio_install.sh
 #######################################################################################
 
 ## create vf 
