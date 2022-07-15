be0ddc4cbe6ef9f557f5f67d6488b5d6701fb3fd
Luo, Lin <lin.luo@intel.com>
2022-07-15 09:20:45 +0000

    add build test

---
+++ build_test.sh
@@ -0,0 +1,20 @@
+if [ $# -ne 3 ] || [ $1 = "-h" ];then
+    echo "
+         example : ./timer_perf.sh  cslsp prod_r21.11 prod_r21.11
+         example : ./timer_perf.sh  iclsp prod_r21.11 prod_r21.11
+         example : ./timer_perf.sh  icld prod_r21.11 prod_r21.11
+     "
+   exit 0
+fi
+platform=$1
+flexran_version=$2
+dpdk_version=$3
+
+base=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
+
+cd $base
+./setup.sh build $flexran_version $dpdk_version
+
+cd $base
+./timer/do_sdk_perf.sh $platform $flexran_version
+./timer/do_pipline_perf.sh $platform $flexran_version
