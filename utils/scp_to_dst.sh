8dbdb9926118e538b1221c17c7248f4dbb8141cc
Luo, Lin <lin.luo@intel.com>
2022-07-20 22:40:40 +0800

    update scp to dst

--- utils/scp_to_dst.sh
+++ utils/scp_to_dst.sh
@@ -3,8 +3,8 @@ dst_ip_folder=$1
 folder=$2
 
 if [ -n "$dst_ip_folder" ]; then
-    echo "---------result scp -r $folder $dst_ip_folder---------"
-    scp -r $folder $dst_ip_folder
+    echo "---------result scp -r $folder/* $dst_ip_folder---------"
+    scp -r $folder/* $dst_ip_folder
 else
     echo "---------result store in local---------"
 fi
